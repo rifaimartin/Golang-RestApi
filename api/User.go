8a7f38bdfe739827df903a74c6953f08f88fdc3b
Rifaimartin <academyyyy12@gmail.com>
2020-02-21 06:33:02 +0700

    SetUp environemt

---
+++ api/User.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"errors"
+	"html"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/badoux/checkmail"
+	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID        uint32    `gorm:"primary_key;auto_increment" json:"id"`
+	Nickname  string    `gorm:"size:255;not null;unique" json:"nickname"`
+	Email     string    `gorm:"size:100;not null;unique" json:"email"`
+	Password  string    `gorm:"size:100;not null;" json:"password"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"created_at"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+}
